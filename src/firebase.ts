f7ccf3021afdd2f39eabffe6a0db370d4a21d6d0
lemon-den <kythien0023@gmail.com>
2026-08-10 05:41:38 +0700

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,13 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBSqqxYr2OAqCats9TvBWCJf5hGKZEpApo",
-  authDomain: "o-roleplay-nha-chanh.firebaseapp.com",
-  projectId: "o-roleplay-nha-chanh",
-  storageBucket: "o-roleplay-nha-chanh.firebasestorage.app",
-  messagingSenderId: "772035995866",
-  appId: "1:772035995866:web:d5686aacb690fa7c069de3",
-  measurementId: "G-JNTQYME8N6"
+  apiKey: "CỤC_API_KEY_CỦA_BÀ",
+  authDomain: "...",
+  projectId: "...",
+  storageBucket: "...",
+  messagingSenderId: "...",
+  appId: "..."
 };
 
 const app = initializeApp(firebaseConfig);
